3a16bf7dc09cc9101b884f5e4e6ab082839197b5
0x0ffff <oxoffff@outlook.com>
2020-06-03 15:03:43 +0800

    invert-binary-tree

---
+++ june-leetcoding-challenge/week1/invert-binary-tree.js
@@ -0,0 +1,52 @@
+// Invert a binary tree.
+
+// Example:
+
+// Input:
+
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+// Output:
+
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+// Trivia:
+// This problem was inspired by this original tweet by Max Howell:
+
+// Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+
+// 反转二叉树
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  var stack = [root];
+
+  while (stack.length) {
+    let n = stack.pop();
+
+    if (!n) {
+      continue;
+    }
+
+    [n.left, n.right] = [n.right, n.left];
+    stack.push(n.left, n.right);
+  }
+  return root;
+};
