f635d3b4baae1897bbdabbd6902b4068c40c20a4
0x0ffff <oxoffff@outlook.com>
2020-06-07 16:13:51 +0800

    coin-change-2

---
+++ june-leetcoding-challenge/week1/coin-change-2.js
@@ -0,0 +1,55 @@
+// You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+
+// Example 1:
+
+// Input: amount = 5, coins = [1, 2, 5]
+// Output: 4
+// Explanation: there are four ways to make up the amount:
+// 5=5
+// 5=2+2+1
+// 5=2+1+1+1
+// 5=1+1+1+1+1
+// Example 2:
+
+// Input: amount = 3, coins = [2]
+// Output: 0
+// Explanation: the amount of 3 cannot be made up just with coins of 2.
+// Example 3:
+
+// Input: amount = 10, coins = [10]
+// Output: 1
+
+// Note:
+
+// You can assume that
+
+// 0 <= amount <= 5000
+// 1 <= coin <= 5000
+// the number of coins is less than 500
+// the answer is guaranteed to fit into signed 32-bit integer
+
+// 给出总和，和硬币的面值，求面值组成总和的组合数
+// 也就是硬币找零
+// 递归会 Time Limit
+// 所以要用 dp
+
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+  let dp = new Array(amount + 1).fill(0);
+
+  dp[0] = 1;
+
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = 1; j <= amount; j++) {
+      if (j >= coins[i]) {
+        dp[j] = dp[j] + dp[j - coins[i]];
+      }
+    }
+  }
+
+  return dp[amount];
+};
